0f7fb25bf8335b5cb943e7faac295552e0f79b52
ilyargat <81382684+ilyargat@users.noreply.github.com>
2021-08-06 14:25:26 +0300

    Add files via upload

---
+++ whoa.py
@@ -0,0 +1,5 @@
+from os import system
+print('Мы перешли в телеграм\nt.me/russia_spam_bot\n')
+a = input('Открыть бота SMS KILLER да/нет >> ')
+if a !='нет':
+    system('termux-open-url https://t.me/russia_spam_bot')
