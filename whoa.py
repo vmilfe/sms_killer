9ccb25be8d7a6bac7164a13dd16f00cc1b38fbda
ilyargat <81382684+ilyargat@users.noreply.github.com>
2021-09-17 10:04:14 +0300

    Update whoa.py

--- whoa.py
+++ whoa.py
@@ -1,5 +1,5 @@
 from os import system
-print('Мы перешли в телеграм\nt.me/russia_spam_bot\n')
+print('Мы перешли в телеграм\nt.me/smskiller_own_bot\n')
 a = input('Открыть бота SMS KILLER да/нет >> ')
 if a !='нет':
-    system('termux-open-url https://t.me/russia_spam_bot')
+    system('termux-open-url https://t.me/smskiller_own_bot')
