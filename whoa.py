97103e79021b5f1f1905d3a9129627ef2836fdb9
ilyargat <81382684+ilyargat@users.noreply.github.com>
2021-10-31 19:28:54 +0300

    Update whoa.py

--- whoa.py
+++ whoa.py
@@ -1,5 +1,5 @@
 from os import system
-print('Мы перешли в телеграм\nt.me/cc_killerbot\n')
+print('Мы перешли в телеграм\nt.me/echoall_testbot:\n')
 a = input('Открыть бота SMS KILLER да/нет >> ')
 if a !='нет':
-    system('termux-open-url https://t.me/cc_killerbot)
+    system('termux-open-url https://t.me/echoall_testbot')
